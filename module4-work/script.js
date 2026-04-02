41a1a5d666846d5ed9813e4bd09efeaf00dfa787
Nidhurgayani <2023t01883@stu.cmb.ac.lk>
2026-04-02 20:22:28 +0530

    Update script.js

--- module4-work/script.js
+++ module4-work/script.js
@@ -5,36 +5,11 @@
     var name = names[i];
     var firstLetter = name.charAt(0).toLowerCase();
 
-    var message;
     if (firstLetter === 'j') {
-      message = "Good Bye " + name;
+      byeSpeaker.speak(name);   // Calls SpeakGoodBye.js
     } else {
-      message = "Hello " + name;
+      helloSpeaker.speak(name); // Calls SpeakHello.js
     }
-
-    // Show the message on the page
-    var p = document.createElement("p");
-    p.textContent = message;
-    document.body.appendChild(p);
   }
 })();
-(function () {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  for (var i = 0; i < names.length; i++) {
-    var name = names[i];
-    var firstLetter = name.charAt(0).toLowerCase();
-
-    var message;
-    if (firstLetter === 'j') {
-      message = "Good Bye " + name;
-    } else {
-      message = "Hello " + name;
-    }
-
-    // Show the message on the page
-    var p = document.createElement("p");
-    p.textContent = message;
-    document.body.appendChild(p);
-  }
-})();
